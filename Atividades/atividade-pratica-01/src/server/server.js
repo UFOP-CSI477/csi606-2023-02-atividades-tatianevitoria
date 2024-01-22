0edfb147aab813368d15af7b66fbc9ca55b04c1f
Tatiane Vitoria <tatianevitoria18@gmail.com>
2024-01-22 11:18:18 -0300

    feat: config cors

--- Atividades/atividade-pratica-01/src/server/server.js
+++ Atividades/atividade-pratica-01/src/server/server.js
@@ -1,19 +1,19 @@
-//const express = require("express");
 const express = require('express');
-//const bodyParser = require("body-parser");
 const db = require("./database/db.js");
-//const routes = require('./routes/main.js');
 const estadosRouter = require('./routes/estados.js');
 const cidadesRouter = require('./routes/cidades.js');
 const tipo_sanguineosRouter = require('./routes/tipos_sanguineos.js');
 const locais_coletaRouter = require('./routes/locais_coleta.js');
 const doacoesRouter = require('./routes/doacoes.js');
 const pessoasRouter = require('./routes/pessoas.js');
+const cors = require('cors');
 
 const PORT= 4444;
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+
 server.use(estadosRouter);
 server.use(cidadesRouter);
 server.use(tipo_sanguineosRouter);
